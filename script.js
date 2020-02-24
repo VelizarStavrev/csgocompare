6440453989d26536b6ab809275c2729ca0c47bc0
VelizarStavrev <velizar.g.stavrev@gmail.com>
2020-02-24 18:41:19 +0200

    testing update

--- script.js
+++ script.js
@@ -8,11 +8,7 @@
 // CSGO STATS USES THIS API
 // http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=<<KEY>>&steamid=<<PROFILEID>>
 
-
-
-// var testing = JSON.parse('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=8A83A42008F83E486594C9D7374CC977&steamid=76561198050060764');
-
-fetch('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=8A83A42008F83E486594C9D7374CC977&steamid=76561198050060764')
+fetch('https://cors-anywhere.herokuapp.com/http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=8A83A42008F83E486594C9D7374CC977&steamid=76561198050060764')
     .then(res => res.json())
     .then((out) => {
         console.log('Output: ', out);
